e33985c99f17b2f3efc7bb6136e9f100ec2e26bf
Stiffpixels <saifimuzammil2@gmail.com>
2024-11-08 18:25:08 +0530

    initial setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "miniworld",
+  description: `Grab the Best Custom Made Miniatures and ready made miniatures
+  perfect for anniversary and birthdays`,
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
