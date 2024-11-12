d5edcdc4ddf4c80177e7812910102c289b49fc25
Vikrant <tiwarivikrant9764@gmail.com>
2024-11-12 11:58:38 +0530

    made the hero section card responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HomeCard from "~/components/home/HomeCard";
 export default function HomePage() {
   return (
     <main className="">
-        <div className="relative flex items-center justify-center md:justify-end min-h-[400px] md:h-[450px] lg:h-[600px] bg-[url('/images/hero-img.svg')] bg-cover bg-center">
+        <div className="relative flex items-center justify-end px-5 min-h-[400px] md:h-[450px] lg:h-[600px] bg-[url('/images/hero-img.svg')] bg-cover bg-center">
             <HomeCard />
         </div>
     </main>
--- src/components/home/HomeCard.tsx
+++ src/components/home/HomeCard.tsx
@@ -3,18 +3,18 @@ import React from "react"
 
 export default function HomeCard(){
 return (
-    <div className="absolute md:right-[30px] bg-[rgba(0,0,0,0.3)] rounded-lg border px-6 py-4 max-w-[400px] flex flex-col gap-y-5">
+    <div className="absolute bg-[rgba(0,0,0,0.3)] rounded-lg border px-6 py-4 w-[90%] max-w-[400px] flex flex-col gap-y-5">
         <div>
-            <h3 className="text-white font-semibold">New Arrival</h3>
-            <h2 className="text-foreground-pink font-bold text-4xl whitespace-nowrap">Discover Our</h2>
-            <h2 className="text-foreground-pink font-bold text-4xl whitespace-nowrap">New Collection</h2>
+            <h3 className="text-xs md:text-base text-white font-semibold">New Arrival</h3>
+            <h2 className="text-3xl md:text-4xl text-foreground-pink font-bold whitespace-nowrap">Discover Our</h2>
+            <h2 className="text-3xl md:text-4xl text-foreground-pink font-bold whitespace-nowrap">New Collection</h2>
         </div>
 
-        <p className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        <p className="text-white text-xs md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
 
         <button
             type="button"
-            className="px-6 py-4 bg-background-pink rounded-lg w-[max-content]"
+            className="px-4 py-2 bg-background-pink rounded-lg w-[max-content] font-semibold text-white text-base md:text-lg"
             onClick={()=>console.log("Buy now clicked")}
         >Buy Now</button>
     </div>
