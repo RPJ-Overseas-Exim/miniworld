457c79390d865095eb3850f2887bfe6fc37754a3
Vikrant <tiwarivikrant9764@gmail.com>
2024-11-19 15:44:49 +0530

    finally implemented the slider successfully

--- src/components/home_page/slider/Slider_Slides.tsx
+++ src/components/home_page/slider/Slider_Slides.tsx
@@ -1,11 +1,13 @@
 "use client"
 import React from "react";
 import Image from "next/image";
+import { ArrowRightIcon } from "lucide-react";
 
 import { Pagination, Autoplay} from "swiper/modules"
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import 'swiper/css/pagination';
+import { poppinsBold, poppinsRegular } from "public/fonts/fonts";
 
 export default function SwiperSlides(){
     const slideImages = [
@@ -15,34 +17,76 @@ export default function SwiperSlides(){
         "/images/slides/slide_4.webp"
     ]
 
-     return (
-        <Swiper
-            className="w-full h-[300px] md:h-[350px]"
-            loop={true}
-            grabCursor={true}
-            draggable={true}
-            modules={[Pagination, Autoplay]}
-            pagination={{clickable: true}}
-            autoplay={{
-                delay: 5000,
-            }}
-            slidesPerView={3}
+    return (
+        <Swiper 
+           modules = {[Pagination, Autoplay]} 
+           loop={true}
+           grabCursor={true}
+           draggable={true}
+           breakpoints={{
+               640: {
+                   slidesPerView: 2,
+                   centeredSlides: false,
+               },
+               1280: {
+                   slidesPerView: 3,
+                   centeredSlides: false,
+               }
+           }}
+           centeredSlides={true}
+           pagination={{clickable: true}}
         >
-        {slideImages.map((value, index)=>(
-            <SwiperSlide key={index} className="relative rounded-xl border-2 border-border-pink overflow-hidden">
-                {/* filter section */}
-                <div className="absolute w-full h-full bg-[rgba(0,0,0,0.2)]"></div>
-                
-                {/* Image section */}
-                <Image 
-                    src={value}
-                    alt="Slide image"
-                    width={100}
-                    height={100}
-                    className="w-full h-full aspect-square"
-                />
-            </SwiperSlide>
-        ))}
+            {slideImages.map((value, index)=>(
+                <SwiperSlide key={index} >
+                    <Slide value={value} />
+                </SwiperSlide>
+            ))}
         </Swiper>
      )
+}
+
+interface SlideProps{
+    value: string;
+}
+
+export function Slide({value}:SlideProps){
+
+    return (
+        <div className="w-full h-full">
+            <div className="relative">
+                <div className="card-info absolute w-full h-full items-end p-4">
+                    <SlideDetails />
+
+                    <div className="w-10 h-10 bg-background-pink p-2 cursor-pointer">
+                        <ArrowRightIcon className="w-full h-full" />
+                    </div>
+                </div>
+            
+                {/* background Image */}
+                <Image 
+                    src={value}
+                    alt={"Image"}
+                    width={100}
+                    height={100}
+                    className="w-full h-full object-center object-cover"
+                />
+            </div>
+        </div>
+    )
+}
+
+export function SlideDetails(){
+    return (
+        <div className="bg-[#ffffff72] backdrop-blur-sm min-w-[] w-[217px] h-[135px] flex items-center justify-center">
+            <div className="text-foreground-muted">
+                <h2 className={`${poppinsRegular.className} font-semibold text-[1.1rem] text-center`}>
+                    01 - Miniature
+                </h2>
+
+                <h2 className={`${poppinsBold.className} text-[1.8rem] text-center`}>
+                    Creative Miniature
+                </h2>
+            </div>
+        </div>
+    )
 }
